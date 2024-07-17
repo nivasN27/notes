0506011d31a3b7d6bf07c24040bb7353ecfe1c04
Nivash N <nivashnandha2002@gmail.com>
2024-07-17 16:46:19 +0530

    fix: ui changes

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,12 +1,10 @@
 import Notes from '../screens/Notes'
-// import Testing from '../components/Testing'
 import '../styles/App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 function App() {
   const router = createBrowserRouter([
     {path:"/", element: <Notes/>},
-    // {path:"/", element: <Testing/>},
   ])
 
   return (
--- src/screens/Notes.jsx
+++ src/screens/Notes.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useRef, useState } from "react";
-import NotesContainer from "../components/NotesContainer";
-import Modal from "../components/Modal";
-import style from './less/notes.module.less'
-import plus from '../assets/images/plusIcon.webp'
-import light from '../assets/images/light.webp'
-import dark from '../assets/images/dark.webp'
-
+import React, { useEffect, useState } from "react";
+import NotesModal from "../components/NotesModal";
+import plusIcon from "../assets/images/plusIcon.webp";
+import deleteIcon from "../assets/images/deleteIcon.webp";
+import sun from "../assets/images/sun.webp";
+import moon from "../assets/images/moon.webp";
+import edit from "../assets/images/editIcon.webp";
+import style from "./less/notes.module.less";
+import BgImg from "../components/BackgroundImage";
 
 const Notes = () => {
-  const [note, setNote] = useState("");
-  const [imageUrls, setImage] = useState([]);
-  const [noteList, setNoteList] = useState([]);
   const [open, setOpen] = useState(false);
-  const [type, setType] = useState('add')
-  const [theme, setTheme] = useState('dark');
-  const uploadRef = useRef();
+  const [noteList, setNoteList] = useState([]);
+  const [note, setNote] = useState({ id: 0, data: "", imgList: [] });
+  const [type, setType] = useState("add");
+  const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
     const storedNotes = JSON.parse(localStorage.getItem("notes"));
@@ -23,106 +22,120 @@ const Notes = () => {
     }
   }, []);
 
-  const handleChange = (value) => {
-    setNote(value.target.value);
-  };
-
-
-  const handleAdd = () => {
-    let updatedNotes;
+  const handleAdd = (note) => {
     setOpen(false);
-    if(type==='add'){
-      if(imageUrls.length === 0){
-        updatedNotes = [...noteList, {data:note}];
-      } else if (note.length === 0) {
-        updatedNotes = [...noteList, {imgList:imageUrls}];
-      } else {
-        updatedNotes = [...noteList, {data:note, imgList:imageUrls}];
+    let updatedNotes;
+    const id = noteList.length === 0 ? 1 : noteList[noteList.length - 1].id + 1;
+    updatedNotes = [...noteList, { ...note, id }];
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
+    setNoteList(updatedNotes);
+  };
+
+  const handleEdit = (note) => {
+    setOpen(false);
+    const updatedNotes = noteList.map((ele) => {
+      if (ele.id === note.id) {
+        return note;
       }
-    if (note.length > 0 || imageUrls.length>0) {
-      localStorage.setItem("notes", JSON.stringify(updatedNotes));
-      setNoteList(updatedNotes);
-      setNote('')
-    }
-    } else {
-      const updatedNotes = noteList.map((e, idx)=>{
-        if(idx===type){
-          return note;
-        }
-        return e;
-      })
-      localStorage.setItem("notes", JSON.stringify(updatedNotes));
-      setNoteList(updatedNotes);
-    }
-  };
-
-  const handleAddImage = () => {
-    uploadRef.current && uploadRef.current.click()
-  }
-
-  const handleUploadImage = () => {
-    const files = uploadRef.current.files;
-    const urls = [];
-    Array.from(files).forEach((file) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        urls.push(reader.result);
-        if (urls.length === files.length) {
-          console.log(urls)
-          setImage(urls);
-        }
-      };
-      reader.readAsDataURL(file);
+      return ele;
     });
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
+    setNoteList(updatedNotes);
   };
 
-  const handleTheme = () => {
-    if(theme === 'dark'){
-      setTheme('light')
-    } else{
-      setTheme('dark')
-    }
-  }
+  const handleDelete = (id) => {
+    const updatedNotes = noteList.filter((ele) => ele.id !== id);
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
+    setNoteList(updatedNotes);
+  };
 
+  const renderContent = (obj, index) => {
+    return (
+      <div
+        className={style.content}
+        onClick={() => {
+          setType("show");
+          setOpen(true);
+          setNote(obj);
+        }}
+      >
+        {obj?.data?.startsWith("http") || obj?.data?.trim().endsWith(".com") ? (
+          <a href={obj.data} target="_blank">
+            {obj?.data}
+          </a>
+        ) : (
+          <p>{obj?.data}</p>
+        )}
+        <div className={style.imgContainer}>
+          {obj?.imgList?.length > 0 &&
+            obj.imgList.map((ele, idx) => <img src={ele} key={`img${idx}`} />)}
+        </div>
+      </div>
+    );
+  };
 
   return (
-    <div className={style.container}>
-      {open ? (
-        <Modal
-          handleChange={handleChange}
-          setOpen={setOpen}
-          handleAdd={handleAdd}
-          handleAddImage={handleAddImage}
-          note={note}
-          type={type}
-          theme={theme}
-          imageUrls={imageUrls}
+    <>
+        <div className={style.font}>
+          <BgImg theme={theme} />
+        </div>
+      <div
+        className={style.container}
+      >
+        <img
+          src={plusIcon}
+          onClick={() => {
+            setOpen(true);
+            setType("add");
+            setNote({ id: 0, data: "", imgList: [] });
+          }}
+          className={style.plusIcon}
         />
-      ) : (
-        <>
-          <div className={style.btnContainer} onClick={() => {
-            setOpen(true)
-            setType('add')
-          }}>
-            <p className={style.btnContent}>Add your notes</p>
-            <img src={plus} alt="icon" className={style.btnIcon} />
-          </div>
-          <NotesContainer
-            list={noteList}
-            setNoteList={setNoteList}
-            setOpen={setOpen}
-            setType={setType}
-            setNote={setNote}
-            theme={theme}
+        <div>
+          <img
+            src={theme==='light' ? moon : sun}
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            className={style.themeChanger}
           />
-        </>
-
-      )}
-      <div className={style.themeIconWrapper}>
-        <img src={theme==='dark'? dark : light} className={style.themeIcon} onClick={handleTheme}/>
+        </div>
+        <div className={style.noteListContainer}>
+          {noteList.map((obj, index) => (
+            <div key={index} className={style.note}>
+              {renderContent(obj, index)}
+              <div className={style.actionContainer}>
+                <img
+                  className={style.editIcon}
+                  src={edit}
+                  alt="editIcon"
+                  onClick={() => {
+                    setOpen(true);
+                    setNote(obj);
+                    setType("edit");
+                  }}
+                />
+                <img
+                  className={style.deleteIcon}
+                  src={deleteIcon}
+                  alt="delIcon"
+                  onClick={() => handleDelete(obj.id)}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        {open && (
+          <div className={style.modalContainer}>
+            <NotesModal
+              setOpen={setOpen}
+              handleAdd={handleAdd}
+              note={note}
+              type={type}
+              handleEdit={handleEdit}
+            />
+          </div>
+        )}
       </div>
-      <input multiple ref={uploadRef} type="file" accept="image/*" style={{display:"none"}} onChange={handleUploadImage} />
-    </div>
+    </>
   );
 };
 
